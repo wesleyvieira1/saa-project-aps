189d06ba83d90aabe2b58233025be5a1862df8f8
wesleyvieira1 <wesley.henrique@academico.ifpb.edu.br>
2022-09-22 20:27:47 -0300

    Add url de exlusão de usuário

--- usuario/urls.py
+++ usuario/urls.py
@@ -1,9 +1,11 @@
 from unicodedata import name
 from django.urls import path
-from .views import listagemUsuariosView, usuarioCreateView, usuarioUpdateView
+from .views import listagemUsuariosView, usuarioCreateView, usuarioUpdateView, usuarioDeleteView
 
+from django.contrib.auth.decorators import login_required
 urlpatterns = [
     path('',listagemUsuariosView.as_view(), name='usuario.index'),
     path('novo/',usuarioCreateView.as_view(), name='usuario.novo'),
-    path('editar/<int:pk>',usuarioUpdateView.as_view(), name='usuario.editar')
+    path('editar/<int:pk>',usuarioUpdateView.as_view(), name='usuario.editar'),
+    path('excluir/<int:pk>',usuarioDeleteView.as_view(), name='usuario.excluir')
 ]
