0a14d3140068124647d36898a1eec6cabf092bc3
wesleyvieira1 <wesley.henrique@academico.ifpb.edu.br>
2022-09-22 20:28:13 -0300

    mudanças teste

--- usuario/models.py
+++ usuario/models.py
@@ -6,17 +6,16 @@ from django.db import models
 class Usuario(models.Model):
     nome = models.CharField(max_length=256)
     #foto = models.ImageField()
-    email = models.CharField(max_length=100)
+    email = models.EmailField(max_length=100)
     cpf = models.CharField(max_length=11)
     rg = models.CharField(max_length=7)
     endereco = models.CharField(max_length=100)
     data_nascimento = models.DateField()
     data_entrada = models.DateField()
     turno = models.CharField(max_length=10)
-    departamento = models.CharField(max_length=20)
+    #departamento = models.CharField(max_length=20)
     senha = models.CharField(max_length=8)
     #historico = models.ImageField()
     #ativo = models.BooleanField()
-    
     def __str__(self) -> str:
         return self.nome
